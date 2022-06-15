15183b17e9fe0d97f4f07cabc2fb8f1427c1cd35
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-14 23:09:16 -0700

    create class files

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
+const enquirer = require("enquirer");
 const { prompt } = require("enquirer"); // https://github.com/enquirer/enquirer
 const { writeFile } = require("fs");
 
-// EMPLOYEE
-// name
-// id
-// email
-// getName()
-// getId()
-// getEmail()
-// getRole()—returns 'Employee'
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-// The other three classes will extend Employee.
+// const addEmployee = () => {
+//   prompt([
+//     {
+//       type: "select",
+//       name: "role",
+//       message: "Which role do you want to add?",
+//       choices: ["Manager", "Engineer", "Intern"],
+//     },
+//     {
+//       type: "input",
+//       name: "name",
+//       message: "What is their full name?",
+//     },
+//     {
+//       type: "input",
+//       name: "id",
+//       message: "What is their ID?",
+//     },
+//     {
+//       type: "input",
+//       name: "email",
+//       message: "What is their email?",
+//     },
+//   ])
+//     .then((answers) => {
+//       let employee = new Employee(
+//         answers.name,
+//         answers.id,
+//         answers.email,
+//         answers.role
+//       );
+//       console.log(employee);
+//     })
+//     .catch(console.error);
+// };
 
-// MANAGER
-// officeNumber
-// getRole() — overridden to return 'Manager'
+const addInfo = (role) => {
+  prompt([])
+    .then((answers) => {
+      let employee = new Employee(answers.name, answers.id, answers.email);
+      console.log(employee);
+      // const readmeContent = generateReadme(answers);
 
-// ENGINEER
-// github—GitHub username
-// getGithub()
-// getRole() — overridden to return 'Engineer'
+      // writeFile('README.md', readmeContent, (err) =>
+      //   err ? console.log(err) : console.log('Successfully created README.md!')
+      // );
+    })
+    .catch(console.error);
+};
 
-// INTERN
-// school
-// getSchool()
-// getRole()—overridden to return 'Intern'
+const testEmployee = new Employee();
+const name = await testEmployee.name;
+console.log(name);
 
 // validation to ensure that user input is in the proper format.
